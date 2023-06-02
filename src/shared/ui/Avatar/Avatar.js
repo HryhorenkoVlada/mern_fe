3d24b92a544a85a1bf82ec95b286883a56bb9e5d
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:27:29 +0300

    feat: add avatar component

---
+++ src/shared/ui/Avatar/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Avatar.scss';
+
+const Avatar = ({ className = '', image, alt, width = 64 }) => {
+  return (
+    <div className={`avatar ${className}`}>
+      <img src={image} alt={alt} style={{ width: width, height: width }} />
+    </div>
+  );
+};
+
+export default Avatar;
