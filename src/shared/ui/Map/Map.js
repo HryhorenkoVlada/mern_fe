18def1fedae25b8d10ab953d7a08a5b897066e73
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:28:31 +0300

    feat: add map component

---
+++ src/shared/ui/Map/Map.js
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from 'react';
+
+import { darkModeMap } from '../../../utils/helpers/darkModeMapStyles';
+
+import './Map.scss';
+
+const Map = ({ center, zoom, className = '', style = {} }) => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center,
+      zoom,
+      styles: darkModeMap,
+    });
+
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
+
+  return <div className={`map ${className}`} style={style} ref={mapRef}></div>;
+};
+
+export default Map;
