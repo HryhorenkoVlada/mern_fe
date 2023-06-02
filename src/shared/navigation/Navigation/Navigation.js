81e0b7e3a09d4412b7bff754d2811c4276c98643
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:43:25 +0300

    feat: add main navigation and link components

---
+++ src/shared/navigation/Navigation/Navigation.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import NavLinks from '../NavLinks/NavLinks';
+import SideDrawer from '../SideDrawer/SideDrawer';
+import { MainHeader } from '../../../layout';
+import { Backdrop } from '../../../shared/ui';
+
+import './Navigation.scss';
+
+const Navigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <>
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+    </>
+  );
+};
+
+export default Navigation;
