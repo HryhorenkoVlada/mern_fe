7175cb51afc7b2d6e40835544e8569aeb6a18eaf
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-03 01:43:11 +0300

    refactor: remove code dublication and unnecessary undefined in classNames

--- src/shared/ui/Button/Button.js
+++ src/shared/ui/Button/Button.js
@@ -4,12 +4,22 @@ import { Link } from 'react-router-dom';
 import './Button.scss';
 
 const Button = (props) => {
+  const classes = [];
+
+  if (props.inverse) {
+    classes.push('button--inverse');
+  }
+
+  if (props.danger) {
+    classes.push('button--danger');
+  }
+
   if (props.href) {
     return (
       <a
-        className={`button button--${props.size || 'default'} ${
-          props.inverse && 'button--inverse'
-        } ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${classes.join(
+          ' '
+        )}`}
         href={props.href}
       >
         {props.children}
@@ -21,9 +31,9 @@ const Button = (props) => {
       <Link
         to={props.to}
         exact={props.exact}
-        className={`button button--${props.size || 'default'} ${
-          props.inverse && 'button--inverse'
-        } ${props.danger && 'button--danger'}`}
+        className={`button button--${props.size || 'default'} ${classes.join(
+          ' '
+        )}`}
       >
         {props.children}
       </Link>
@@ -31,9 +41,9 @@ const Button = (props) => {
   }
   return (
     <button
-      className={`button button--${props.size || 'default'} ${
-        props.inverse && 'button--inverse'
-      } ${props.danger && 'button--danger'}`}
+      className={`button button--${props.size || 'default'} ${classes.join(
+        ' '
+      )}`}
       type={props.type}
       onClick={props.onClick}
       disabled={props.disabled}
