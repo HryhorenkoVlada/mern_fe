bde613e0d76dcf3533d662dff75c88711182e1bd
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:27:46 +0300

    feat: add sidedrawer component

---
+++ src/shared/navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import './SideDrawer.scss';
+
+const SideDrawer = ({ children, show, onClick }) => {
+  const nodeRef = useRef(null);
+
+  const content = (
+    <CSSTransition
+      in={show}
+      timeout={500}
+      classNames="slide-in-left"
+      nodeRef={nodeRef}
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside ref={nodeRef} className="side-drawer" onClick={onClick}>
+        {children}
+      </aside>
+    </CSSTransition>
+  );
+
+  return ReactDOM.createPortal(content, document.getElementById('drawer'));
+};
+
+export default SideDrawer;
