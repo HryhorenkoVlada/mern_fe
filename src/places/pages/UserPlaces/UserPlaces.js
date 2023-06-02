87a0b35b15a9a583daea5ea8b771e0add7c9502c
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:46:51 +0300

    feat: add page for randering users lists

---
+++ src/places/pages/UserPlaces/UserPlaces.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { PlacesList } from '../../components';
+import { DUMMY_PLACES } from '../../../utils/data/places';
+
+const UserPlaces = () => {
+  const { userId } = useParams();
+
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  return <PlacesList places={loadedPlaces} />;
+};
+
+export default UserPlaces;
