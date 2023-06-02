22b45e2ffa548b5a7e3c36a11f17a7615d234d22
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-02 22:30:39 +0300

    feat: add index entry file for ui components

---
+++ src/shared/ui/index.js
@@ -0,0 +1,7 @@
+export { default as Avatar } from './Avatar/Avatar';
+export { default as Card } from './Card/Card';
+export { default as Backdrop } from './Backdrop/Backdrop';
+export { default as Button } from './Button/Button';
+export { default as Modal } from './Modal/Modal';
+export { default as Map } from './Map/Map';
+export { default as Input } from './Input/Input';
