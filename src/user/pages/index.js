494201b412413ab7a7fbf541ccfc5d33be47ede6
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-06-05 15:42:20 +0300

    refactor: change users pages structure

---
+++ src/user/pages/index.js
@@ -0,0 +1,2 @@
+export { default as SignIn } from './auth/SignIn.js';
+export { default as Users } from './users/Users.js';
--- src/user/pages/users/Users.js
+++ src/user/pages/users/Users.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { UsersList } from '../components';
-import { DUMMY_USERS } from '../../utils/data/users';
+import { UsersList } from '../../components';
+import { DUMMY_USERS } from '../../../utils/data/users';
 
 const Users = () => {
   return <UsersList usersList={DUMMY_USERS} />;
